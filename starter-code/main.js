b102250441a2c70b5a551807e670b5bf71a2db1a
The Buffalo <ZeroCalm@Thes-MacBook-Pro.local>
2016-11-29 12:23:54 -0800

    updated logic if/else

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,6 +1,19 @@
-/* Here are the cards */
+/*   The Memory Game */
 
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
-var cardFour= "king";
+var cardFour = "king";
+
+
+if(cardOne === cardTwo){
+	alert("You found a match");
+}
+
+else if (cardThree === cardFour){
+alert("You found a match");
+}
+
+else if (cardOne === cardFour){
+	alert("Sorry, try again.")
+}

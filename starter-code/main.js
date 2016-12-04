142cea93418e0d29384ed019d4f79c4c669da3a3
The Buffalo <ZeroCalm@Thes-MacBook-Pro.local>
2016-12-04 14:58:09 -0800

    Unit 10 Assignment

--- starter-code/main.js
+++ starter-code/main.js
@@ -6,6 +6,36 @@ var cardThree = "king";
 var cardFour = "king";
 
 
+
+
+var createCards = function(){
+
+var gameBoard = document.getElementById('game-board');
+
+for(var i = 1; i <= 4; i++){
+
+  //Create newCard as a Div 
+
+	var newCard = document.createElement('div');
+
+  //NewCard now has a class of "card"	
+	newCard.className = 'card';
+	
+  // Append card to the board	
+	gameBoard.appendChild(newCard);
+	}
+}
+
+createCards();
+
+
+
+
+
+/*
+}
+
+
 if(cardOne === cardTwo){
 	alert("You found a match");
 }
@@ -17,3 +47,5 @@ alert("You found a match");
 else if (cardOne === cardFour){
 	alert("Sorry, try again.")
 }
+
+*/
